801fbaa530ef024bf4182e7bb61f358933407061
josher <josher01.lin@gmail.com>
2017-12-08 14:38:14 +0800

    add back-to-list button in create and edit form view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   validates_presence_of :res_name, :res_opening_hours, :res_tel, :res_address, :res_description
   mount_uploader :photo, PhotoUploader
   max_paginates_per 10
-  belongs_to :category
+  belongs_to :category, optional: true
 end
