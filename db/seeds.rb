7b2027cca0ffe5785ca9c18eb113bebd99ed3175
josher <josher01.lin@gmail.com>
2017-12-27 14:22:02 +0800

    fine-tune view of restaurants listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 
 #Category
-Category.destroy_all
 
+Category.destroy_all
 category_list =[
   { name: "中式料理" },
   { name: "日本料理" },
@@ -24,7 +24,5 @@ category_list.each do |category|
   Category.create( name: category[:name] )
 end
 puts "Category created!"
-
-#Restaurants photo
-
+Restaurants photo
 
