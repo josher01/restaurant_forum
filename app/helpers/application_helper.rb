39458c49a373b24264bca9b97c9477b2ba82037d
josher <josher01.lin@gmail.com>
2018-01-17 18:21:56 +0800

    add favorited restaurants, followings and followers to user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,21 +15,21 @@ module ApplicationHelper
 
   def avatar_generator(x)
     if x == 0
-      image_tag "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png", class: "responsive-img circle"
     elsif x == 1
-      image_tag "https://pickaface.net/gallery/avatar/20160303_155723_4728_Pablo.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/20160303_155723_4728_Pablo.png", class: "responsive-img circle"
     elsif x == 2
-      image_tag "https://pickaface.net/gallery/avatar/20140304_222239_3444_jail.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/20140304_222239_3444_jail.png", class: "responsive-img circle"
     elsif x == 3
-      image_tag "https://pickaface.net/gallery/avatar/20130211_084453_4162_Jeff.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/20130211_084453_4162_Jeff.png", class: "responsive-img circle"
     elsif x == 4
-      image_tag "https://pickaface.net/gallery/avatar/20141014_233058_2523_CarsonJr.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/20141014_233058_2523_CarsonJr.png", class: "responsive-img circle"
     elsif x == 5
-      image_tag "https://pickaface.net/gallery/avatar/unr_elizabeth_170922_0009_zbii0.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/unr_elizabeth_170922_0009_zbii0.png", class: "responsive-img circle"
     elsif x == 6
-      image_tag "https://pickaface.net/gallery/avatar/unr_susan_171105_1538_gi.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/unr_susan_171105_1538_gi.png", class: "responsive-img circle"
     else
-      image_tag "https://pickaface.net/gallery/avatar/20121023_152128_1823_gasperin.png", class: "responsive-img"
+      image_tag "https://pickaface.net/gallery/avatar/20121023_152128_1823_gasperin.png", class: "responsive-img circle"
     end
   end
 end
