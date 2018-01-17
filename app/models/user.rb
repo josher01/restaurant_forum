e2470057860fa6996ba3a9de41f57d16357ccd71
josher <josher01.lin@gmail.com>
2018-01-17 15:55:15 +0800

    setup user has many followers and define the followers? method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,15 @@ class User < ApplicationRecord
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
 
+  has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+
   def followings?(user)
     self.followings.include?(user)
+  end
+
+  def followers?(user)
+    self.followers.include?(user)
   end
 
 end
